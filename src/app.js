9724ab4b30cfd651d377a2d72d6f4f761ac22045
revealdone <muhamadwahidromdoni@gmail.com>
2025-01-26 22:11:59 +0700

    asd

--- src/app.js
+++ src/app.js
@@ -68,6 +68,64 @@ document.addEventListener("alpine:init", () => {
   });
 });
 
+// disabled checkout
+const checkoutButton = document.querySelector(".checkout-button");
+checkoutButton.disabled = true;
+
+const form = document.querySelector("#checkoutForm");
+
+form.addEventListener("keyup", function () {
+  for (let i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].value.length !== 0) {
+      checkoutButton.classList.remove("disabled");
+      checkoutButton.classList.add("disabled");
+    } else {
+      return false;
+    }
+  }
+
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove("disabled");
+});
+
+// kirim data ketika checkout di klik
+checkoutButton.addEventListener("click", async function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = new URLSearchParams(formData);
+  const objData = Object.fromEntries(data);
+  const message = formatMessage(objData);
+  window.open("http://wa.me/081808866422?text=" + encodeURIComponent(message));
+
+  // minta transaksi lewat ajax
+  // try {
+  //   const response = await fetch("php/order.php", {
+  //     method: "POST",
+  //     body: data,
+  //   });
+  //   const token = await response.text();
+  //   // console.log(token);
+  //   window.snap.pay(token);
+  // } catch (error) {
+  //   console.log(error.message);
+  // }
+});
+
+// format pesan whatsappc
+const formatMessage = (obj) => {
+  return `Data Customer
+    Nama: ${obj.name}
+    Email: ${obj.email}
+    No HP: ${obj.phone}
+    
+Data Pesanan
+  ${JSON.parse(obj.items).map(
+    (item) => `${item.name} (${item.quantity} x ${rupiah(item.total)})\n`
+  )}
+  TOTAL: ${rupiah(obj.total)}
+  Terima kasih.`;
+};
+
 // uang rupiah
 const rupiah = (number) => {
   return new Intl.NumberFormat("id-ID", {
